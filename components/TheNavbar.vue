7d740a435aeda3aafc2b21884879859baacce5c5
Alireza Arabshahi <arabshahi.alireza@outlook.com>
2021-09-02 23:44:23 +0430

    TheNavabr Created

---
+++ components/TheNavbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="bg-danger">
+    <ul>
+      <!-- <li class="mr-8 mb-6 lg:mb-0">
+        <mega-menu />
+      </li> -->
+      <li>
+        <NuxtLink to="/">سوپر مارکت</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+nav {
+  margin: 0;
+}
+/* .NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+} */
+</style>
