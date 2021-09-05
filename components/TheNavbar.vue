f0796e458e7b2dbe1ef0a29e7c9bb8e683be52ac
Alireza Arabshahi <arabshahi.alireza@outlook.com>
2021-09-05 20:55:53 +0430

    category items added

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,28 +1,67 @@
 <template>
-  <nav class="bg-danger">
-    <ul>
-      <!-- <li class="mr-8 mb-6 lg:mb-0">
+  <nav :class="$style.nav">
+    <ul :class="$style['nav-category-list']">
+      <!-- <li>
         <mega-menu />
       </li> -->
       <li>
+        <NuxtLink to="/">دسته‌بندی کالاها</NuxtLink>
+      </li>
+      <li>
         <NuxtLink to="/">سوپر مارکت</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">تخفیف‌ها و پیشنهاد‌ها</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">دیجی‌کالای من</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">دیجی‌پلاس</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">دیجی‌کلاب</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/">دیجی‌پی</NuxtLink>
       </li>
     </ul>
   </nav>
 </template>
 
-<style>
-nav {
-  margin: 0;
+<style module>
+.nav {
+  background-color: white;
+  /* font-size: 3rem; */
+  box-shadow: 0 2px 4px 0 rgba(113, 113, 113, 5%);
 }
+.nav-category-list {
+  display: flex;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.nav-category-list > li {
+  display: block;
+  list-style: none;
+  padding-left: 1rem;
+  font-size: 0.857rem;
+}
+
+.nav-category-list > li > a {
+  color: #616161;
+  display: block;
+  padding: 7px 12px 8px;
+  text-decoration: none;
+}
+
 /* .NuxtLogo {
   animation: 1s appear;
   margin: auto;
-}
+} */
 
 @keyframes appear {
   0% {
     opacity: 0;
   }
-} */
+}
 </style>
