5c44df940eb1290b7b57164113f3ad72142957d6
Alireza Arabshahi <arabshahi.alireza@outlook.com>
2021-09-07 23:54:48 +0430

    added container class

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <TheNavbar />
   </div>
 </template>
@@ -7,3 +7,12 @@
 <script>
 export default {};
 </script>
+<style>
+.container {
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 15px;
+  padding-left: 15px;
+  max-width: 1676px;
+}
+</style>
